ffcd50a23c1cfe30b0300b0c1dced19f1f106f29
datnt700 <81250707+datnt700@users.noreply.github.com>
2022-06-29 15:20:45 +0200

    hello

--- projet/rock_paper_scissors.py
+++ projet/rock_paper_scissors.py
@@ -17,3 +17,22 @@ def qui_gagne(p, o):
 
 
 print(jeu())
+print()
+# helo chu bo doi cu ho
+def jeu():
+    user = input()
+    computer = random.choice(["r", "p", "s"])
+    if user == computer:
+        return "It's tie"
+    if qui_gagne(user, computer):
+        return "User est gagne"
+    return "User est Perdu"
+
+
+def qui_gagne(p, o):
+    if p == "r" and o == "p" or p == "p" and o == "s" or p == "s" and o == "r":
+        return True
+
+
+print(jeu())
+print()
