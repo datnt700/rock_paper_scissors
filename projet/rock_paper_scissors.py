e09f0f3b87ee749fc0c07aaf4f8326e2bfaeff67
datnt700 <81250707+datnt700@users.noreply.github.com>
2022-06-29 14:37:31 +0200

    python

---
+++ projet/rock_paper_scissors.py
@@ -0,0 +1,19 @@
+import random
+
+
+def jeu():
+    user = input()
+    computer = random.choice(["r", "p", "s"])
+    if user == computer:
+        return "It's tie"
+    if qui_gagne(user, computer):
+        return "User est gagne"
+    return "User est Perdu"
+
+
+def qui_gagne(p, o):
+    if p == "r" and o == "p" or p == "p" and o == "s" or p == "s" and o == "r":
+        return True
+
+
+print(jeu())
